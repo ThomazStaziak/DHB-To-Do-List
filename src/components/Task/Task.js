c39ab3ca89cb1f2adb9f470239d0da0c8d76c1e1
ThomazStaziak <thostaziak@gmail.com>
2019-08-04 14:09:00 -0300

    adding the task component

---
+++ src/components/Task/Task.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+import "./Task.css";
+
+class Task extends Component {
+  render() {
+    return (
+      <div className={this.props.className}>
+        <span>{this.props.title}</span>
+        <span
+          className={this.props.statusClassName}
+          onClick={this.props.statusClick}
+        >
+          {this.props.status}
+        </span>
+      </div>
+    );
+  }
+}
+
+export default Task;
