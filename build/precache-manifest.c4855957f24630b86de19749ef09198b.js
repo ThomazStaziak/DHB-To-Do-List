8dae1aabfaecc8bf6462d11cd9d0218cc6d3829d
ThomazStaziak <thostaziak@gmail.com>
2019-08-06 11:14:08 -0300

    adding settings to transform a pwa and make deploy

---
+++ build/precache-manifest.c4855957f24630b86de19749ef09198b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f32bd9dadc5211c80c7cce66fde4a203",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7665a96981d4e8be5bab",
+    "url": "/static/css/main.0854fd66.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "7665a96981d4e8be5bab",
+    "url": "/static/js/main.8606f0db.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
